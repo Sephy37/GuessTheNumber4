123ad31956f7ddf51c8237d0d0caeb92cff1094f
Sephy37 <146596317+Sephy37@users.noreply.github.com>
2023-10-09 23:34:09 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let remainingGuesses = 0
+let ultraLevelGuesses = 2
+let hardLevelGuesses = 5
+let mediumLevelGuesses = 10
+let easyLevelGuesses = 15
+let numberOfGuesses = 0
+let guess
+let MAX_BOUNDS
+let MIN_BOUNDS
+let gamsStatusLoser = "COMPUTER WON!"
+let gameStatusWinner = "PLAYER WON!"
