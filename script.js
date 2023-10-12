aedcb72198bc51d17b5f0e1e5189434ac3701d2a
Sephy37 <146596317+Sephy37@users.noreply.github.com>
2023-10-13 00:33:15 +0100

    added functions

--- script.js
+++ script.js
@@ -29,3 +29,96 @@ let difficultyLevels = {
     ultra: ultraLevelGuesses
 }
 
+let rand = document.getElementById("randomNum")
+rand.innerHTML = randomNumber
+document.getElementById("randomNum").style.display = "none"
+console.log(randomNumber)
+
+let msg = document.getElementById("message")
+let gameOver = document.getElementById("borders")
+
+let easy = document.getElementById("easyBtn")
+let medium = document.getElementById("mediumBtn")
+let hard = document.getElementById("hardBtn")
+let ultra = document.getElementById("ultraBtn")
+
+let guessBtn = document.getElementById("btn1")
+let resetGuessBtn = document.getElementById("btn2")
+let resetFieldBtn = document.getElementById("btn3")
+
+easy.addEventListener("click",function(){
+    checkWhichButtonWasPressed(easy)
+    displayDifficultLevel(easyLevelGuesses)
+})
+
+medium.addEventListener("click",function(){
+    checkWhichButtonWasPressed(medium)
+    displayDifficultLevel(mediumLevelGuesses)
+})
+
+hard.addEventListener("click",function(){
+    checkWhichButtonWasPressed(hard)
+    displayDifficultLevel(hardLevelGuesses)
+})
+
+ultra.addEventListener("click",function(){
+    checkWhichButtonWasPressed(ultra)
+    displayDifficultLevel(ultraLevelGuesses)
+})
+
+function displayDifficultLevel(difficulty){
+    remainingGuesses = difficulty
+    let guess = document.getElementById("counter")
+    guess.innerHTML = remainingGuesses
+    return remainingGuesses
+}
+
+guessBtn.addEventListener("click",function(){
+    if(guessOutOfBoundsCheck(0,3) && checkForEmptyGuess()){
+        checkGuess()
+    }
+},false)
+
+resetGuessBtn.addEventListener("click",function(){
+    resetGuessBtn()
+})
+
+resetFieldsBtn.addEventListener("click",function(){
+    resetFields()
+})
+
+function checkWhichButtonWasPressed(event){
+    for(let i = 0; i < buttons.length; i++){
+        if(event.id != buttons[i]){
+            document.getElementById(button[i].disabled = true)
+        }
+    }
+}
+
+function guessOutOfBoundsCheck(MIN_BOUNDS,MAX_BOUNDS){
+    let check = false
+    let guess = document.getElementById("guessBox").value
+    if(guess < MIN_BOUNDS || guess > MIN_BOUNDS){
+        check = false
+    }else{
+        check = true
+    }
+    return check
+}
+
+
+function checkForEmptyGuess(){
+    let check = false
+    guess = document.getElementById("guessBox").value.trim()
+    if(guess ===""){
+        showMessage("GUESS CANNOT BE EMPTY!")
+        check = false
+    }else{
+        check = true
+    }
+    return check
+}
+
+function checkGuess(){
+    
+}
